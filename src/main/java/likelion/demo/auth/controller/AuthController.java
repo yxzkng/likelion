79065e3b9fd2b02ede2f7cf2156363f354d1e937
yxzkng <yunjin4809@naver.com>
2026-07-04 15:12:41 +0900

    remove: test file 삭제

--- src/main/java/likelion/demo/auth/controller/AuthController.java
+++ src/main/java/likelion/demo/auth/controller/AuthController.java
@@ -1,23 +1,10 @@
 package likelion.demo.auth.controller;
 
-import jakarta.servlet.http.HttpSession;
-import likelion.demo.global.common.ApiResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 임시 로그인 컨트롤러 (테스트용)
- * - auth 팀이 실제 구현으로 교체할 예정
- */
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-
-    @PostMapping("/test-login")
-    public ResponseEntity<ApiResponse<String>> testLogin(HttpSession session) {
-        session.setAttribute("memberId", 1L);
-        return ResponseEntity.ok(ApiResponse.success(200, "테스트 로그인 성공", "memberId=1 세션 저장됨"));
-    }
+    // TODO: auth 팀이 회원가입, 로그인, 로그아웃 구현 예정
 }
